4a8867d8e19872c6bc1d2e4d467bd03a0989951f
NekoYo1 <kostyafear203@gmail.com>
2022-10-29 12:02:10 +1000

    картинки

--- WPFFILIPPOV_PR10/MainWindow.xaml.cs
+++ WPFFILIPPOV_PR10/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace WPFFILIPPOV_PR10
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void DeleteButtton_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Remove(textBox1.Text);
+        }
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Content", System.ComponentModel.ListSortDirection.Ascending));
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Remove(textBox1.Text);
+        }
     }
 }
